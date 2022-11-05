913521ff75270cd79af07b5f352c471519b24676
Sunil Joshi <joshisunil2601@gmail.com>
2022-11-05 19:57:16 +0530

    inital commit

---
+++ src/App.jsx
@@ -0,0 +1,8 @@
+import { useState } from "react";
+function App() {
+  const [count, setCount] = useState(0);
+
+  return <div className="bg-red-500 text-white">hello word</div>;
+}
+
+export default App;
